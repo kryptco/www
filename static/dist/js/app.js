41eafeeb6071666a8307edd226b0db7232baf736
Petr Kaplunovich <yashikaren@gmail.com>
2016-10-03 22:00:09 -0400

    basic layout for FAQ page

--- static/dist/js/app.js
+++ static/dist/js/app.js
@@ -5,6 +5,10 @@ var _SomeComponent = require('./components/SomeComponent');
 
 $(function () {
     $('#get-started-button').on('click', scrollToGetStartedSection);
+    $('.FAQ__question').on('click', function () {
+        $(this).toggleClass('open');
+        $(this).find('.FAQ__question__answer').slideToggle(500, 'easeInOutQuad');
+    });
 });
 
 function scrollToGetStartedSection() {
--- static/src/js/app.js
+++ static/src/js/app.js
@@ -2,6 +2,10 @@ import {SomeComponent} from './components/SomeComponent';
 
 $(function () {
     $('#get-started-button').on('click', scrollToGetStartedSection);
+    $('.FAQ__question').on('click', function() {
+        $(this).toggleClass('open');
+        $(this).find('.FAQ__question__answer').slideToggle(500, 'easeInOutQuad');
+    });
 });
 
 function scrollToGetStartedSection () {
